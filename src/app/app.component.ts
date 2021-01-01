b388f0dd13ed6c163ba8d798fd77c2973216a924
kaushik <dev.kaushik160@gmail.com>
2021-01-01 14:44:38 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,47 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-video-player-example';
+  videoSource = '/assets/Video/fullhd_video.mp4'
+  videoItems = [
+    {
+      name: 'Video one',
+      src: '/assets/Video/fullhd_video.mp4',
+      type: 'video/mp4'
+    },
+
+  ];
+
+  activeIndex = 0;
+  currentVideo = this.videoItems[this.activeIndex];
+  data;
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+  videoPlayerInit(data) {
+    this.data = data;
+
+    this.data.getDefaultMedia().subscriptions.loadedMetadata.subscribe(this.initVdo.bind(this));
+    this.data.getDefaultMedia().subscriptions.ended.subscribe(this.nextVideo.bind(this));
+  }
+
+  nextVideo() {
+    this.activeIndex++;
+
+    if (this.activeIndex === this.videoItems.length) {
+      this.activeIndex = 0;
+    }
+
+    this.currentVideo = this.videoItems[this.activeIndex];
+  }
+
+  initVdo() {
+    this.data.play();
+  }
+
+  startPlaylistVdo(item, index: number) {
+    this.activeIndex = index;
+    this.currentVideo = item;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {VgCoreModule} from '@videogular/ngx-videogular/core';
+import {VgControlsModule} from '@videogular/ngx-videogular/controls';
+import {VgOverlayPlayModule} from '@videogular/ngx-videogular/overlay-play';
+import {VgBufferingModule} from '@videogular/ngx-videogular/buffering';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    VgCoreModule,
+    VgControlsModule,
+    VgOverlayPlayModule,
+    VgBufferingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
