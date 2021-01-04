388a76d69d2aeaa9d89df635f8854903dfbb5d2d
kaushik <dev.kaushik160@gmail.com>
2021-01-04 15:58:54 +0530

    committed changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, ElementRef, HostListener, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
@@ -6,30 +6,45 @@ import { DomSanitizer } from '@angular/platform-browser';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-video-player-example';
-  con_width: any = "auto";
-  con_height: any = "auto";
-  vid: any;
-  videoSource = '/assets/Video/fullhd_video.mp4'
-  activeIndex = 0;
-  @HostListener('window:resize', ['$event'])
-  onResize(event) {
-    this.con_width = event.target.innerWidth + "px";
-    this.con_height = event.target.innerHeight + "px";
+export class AppComponent implements OnInit, OnDestroy {
+  @ViewChild('cropCvs', { static: true }) canvas: ElementRef<HTMLCanvasElement>;
+  ctx: CanvasRenderingContext2D;
+  @ViewChild('homepagevideo', { static: false }) homepage_video;
+  interval: any;
 
-  }
+  title = 'angular-video-player-example';
+  videoSource = '/assets/Video/1280 uncropped 02.mp4'
+  videoResSource = "/assets/Video/fullhd_video.mp4"
+  activeIndex = 0;
 
   videoTag;
+  videoCanvasTag: any;
 
+  // ctx: any;
   constructor(private sanitizer: DomSanitizer) {
     this.videoTag = this.getVideoTag();
+
 
   }
 
   ngOnInit(): void {
-    this.con_width = window.innerWidth + "px";
-    this.con_height = window.innerHeight + "px";
+    this.videoCanvasTag = this.getVideoCanvasTag();
+    this.ctx = this.canvas.nativeElement.getContext('2d');
+    setTimeout(() => {
+      this.loop();
+    }, 1000);
+  }
+
+  loop() {
+    let video: any = document.getElementById('homepage-video');
+    if (this.ctx) {
+      this.ctx.drawImage(video, 0, 0, 1280, 720, 0, 0, 1280, 690);
+      setTimeout(this.loop, 1000 / 30);
+      this.interval = setInterval(() => {
+        this.loop();
+      }, 200)
+    }
+
   }
 
   private getVideoTag() {
@@ -40,4 +55,24 @@ export class AppComponent {
         </video>`
     );
   }
+
+  private getVideoCanvasTag() {
+    return this.sanitizer.bypassSecurityTrustHtml(
+      `<video #homepagevideo style="visibility: hidden;height: 0;width: 0;" id="homepage-video" class="video-js vjs-default-skin" muted autoplay loop controls preload="auto"
+      data-setup='{ "responsive": true, "aspectRatio": "16:9" }' webkit-playsinline>
+      <source src="${this.videoSource}"
+        type="video/mp4">
+      <p class="vjs-no-js">
+        To view this video please enable JavaScript, and consider upgrading to a web browser that
+        <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
+      </p>
+    </video>`
+    );
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.interval)
+  }
+
+
 }
